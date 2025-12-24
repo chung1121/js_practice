893dfb797333ac0e5d095cc38dd0ed982dd28ad7
chung1121 <akira.kira0128@gmail.com>
2025-12-24 19:04:05 +0900

    todolist

--- src/js/modules/todo.js
+++ src/js/modules/todo.js
@@ -1,1 +1,11 @@
-console.log('from todo.js');
+const addForm = document.querySelector('.td-add-form');
+const addInput = document.querySelector('.td-add-input');
+
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let todoObj = {
+    content: addInput.value.trim(),
+    isDone: false,
+  };
+  console.log(todoObj);
+});
